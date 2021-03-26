8f21776c1e3c0476d332edef57b03aaf30a7e2b1
zamieabdullah <zamieabdullah0@gmail.com>
2021-03-26 15:29:31 -0400

    startScene created and reorganized everything into folders

---
+++ SandCastle_Defense/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+    
+    public void endGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
