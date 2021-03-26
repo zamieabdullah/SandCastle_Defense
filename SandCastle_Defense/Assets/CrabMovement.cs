8ebb2182d0f8f01c23253f42e4295ca277662f91
emai-lai <emai.lai@tufts.edu>
2021-03-26 14:40:12 -0400

    added crabs that move towards sandcastle center
    - crab is a prefab / there are 3 of them with different starting points, some off screen / they currently all move towards (0, 2, 0)

---
+++ SandCastle_Defense/Assets/CrabMovement.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabMovement : MonoBehaviour
+{
+ 	private Vector3 target = new Vector3(0, 2, 0);
+ 	private float speed = 1;
+	// float wiggleDistance = 1;
+	// float wiggleSpeed = 5;
+	 
+	void Update()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+
+	    // float xPosition = Mathf.Sin(Time.time * wiggleSpeed) * wiggleDistance;
+
+	    // transform.localPosition = new Vector3(xPosition, 0, 0);
+	}
+
+
+}
+
--- SandCastle_Defense/Assets/PlayerController.cs
+++ SandCastle_Defense/Assets/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
 	public float kidSpeed = 5f;
