eb546068bada3a9c1f448755bfd6ebfb6cc21b99
zamieabdullah <zamieabdullah0@gmail.com>
2021-03-29 01:18:46 -0400

    Player animation
    Player animation and movement has been updated

--- SandCastle_Defense/Assets/Scripts/PlayerController.cs
+++ SandCastle_Defense/Assets/Scripts/PlayerController.cs
@@ -10,8 +10,10 @@ public class PlayerController : MonoBehaviour
 
 	public Rigidbody2D rb;
 
-    public TextMeshProUGUI sanddollarCountText;
-    private int sanddollarCount;
+  public TextMeshProUGUI sanddollarCountText;
+	public Animator animator;
+  private int sanddollarCount;
+	private bool looking_right = true;
 
 
 	Vector2 movement;
@@ -25,13 +27,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal") * kidSpeed;
+        movement.y = Input.GetAxisRaw("Vertical") * kidSpeed;
+				
+				animator.SetFloat("Speed", Mathf.Abs(movement.x));
+				
+				if (movement.x < 0 && looking_right) {
+				    Flip();
+				} else if (movement.x > 0 && !looking_right) {
+					  Flip();
+				}
     }
 
     void FixedUpdate()
     {
-    	rb.MovePosition(rb.position + movement * kidSpeed * Time.fixedDeltaTime);
+    	rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -48,6 +58,17 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+		
+		private void Flip()
+				{
+						// Switch the way the player is labelled as facing.
+						looking_right = !looking_right;
+
+						// Multiply the player's x local scale by -1.
+						Vector3 theScale = transform.localScale;
+						theScale.x *= -1;
+						transform.localScale = theScale;
+				}
 
     void SetSanddollarCountText()
     {
