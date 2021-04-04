7b16f5d24709228406d4a3c35bfe50654d210e06
emai-lai <emai.lai@tufts.edu>
2021-04-04 01:16:18 -0400

    Dig script - press space bar to make sand darker
    (it essentially deletes the current tile and reveals the darker "Tilemap_BBG" tile underneath.) still have to make it so it only works if player is holding shovel... and associate some kind of collider with it so it does something to the crabs/water

---
+++ SandCastle_Defense/Assets/Scripts/Dig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Dig : MonoBehaviour
+{
+	
+    public Tilemap tilemap;
+    public GameObject player;
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            //print("space key was pressed");
+        	DigTrench();
+        }
+    }
+
+    void DigTrench()
+    {
+        // get current grid location
+        Vector3Int currCell = tilemap.WorldToCell(player.transform.position);
+
+        // delete the tile there
+        tilemap.SetTile(currCell, null);
+
+    }
+}
