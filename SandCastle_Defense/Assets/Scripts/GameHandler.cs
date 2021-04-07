7fab6fb4e5636b9529aa68f8bba2ba86b71aee30
zamieabdullah <zamieabdullah0@gmail.com>
2021-04-07 11:16:19 -0400

    Restart and end buttons
    Winning and Losing Scenes now have restart game button and end game.

--- SandCastle_Defense/Assets/Scripts/GameHandler.cs
+++ SandCastle_Defense/Assets/Scripts/GameHandler.cs
@@ -25,7 +25,11 @@ public class GameHandler : MonoBehaviour
     {
         if (Input.GetKey("escape"))
         {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
             Application.Quit();
+            #endif
         }
     }
 
