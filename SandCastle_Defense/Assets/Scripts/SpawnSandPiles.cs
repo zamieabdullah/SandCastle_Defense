86ddb78d3f54238969b36530966013ab7a8e23b4
emai-lai <emai.lai@tufts.edu>
2021-04-17 12:52:25 -0400

    sandpiles spawn randomly
    start with 3, and then new one spawns every 8f... also they only spawn at the left and right sides of the screen (not the middle)

---
+++ SandCastle_Defense/Assets/Scripts/SpawnSandPiles.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSandPiles : MonoBehaviour
+{
+    public GameObject sandpile;
+    public int numToStart = 3;
+    public GameObject parent;
+
+    private float spawnInterval = 8f;
+    private float tempTime = 0f;
+
+    public System.Random rand = new System.Random();
+
+    void Start()
+    {
+        for(int i = 0; i < numToStart; i++)
+        {
+            spawnSandpile();
+        }
+    }
+
+    void Update()
+    {
+        tempTime += Time.deltaTime;
+        while(tempTime >= spawnInterval) 
+        {
+          spawnSandpile();
+          tempTime -= spawnInterval;
+        }
+    }
+
+    private void spawnSandpile()
+    {
+        GameObject a = Instantiate(sandpile) as GameObject;
+
+        if (rand.Next(0, 2) == 0)
+        {
+            // left edge
+            a.transform.position = new Vector2(Random.Range(-11f, -6f), Random.Range(-3f, 6f));
+        }
+        else
+        {
+            // right edge
+            a.transform.position = new Vector2(Random.Range(6f, 11f), Random.Range(-3f, 6f));
+        }
+
+        a.transform.SetParent(parent.transform);
+    }
+}
