8097c0cf746f3fdc5a4d42d6b1b0a32e2db2de0e
ccarney10 <79951871+ccarney10@users.noreply.github.com>
2021-04-24 15:20:27 -0400

    Added box collider to stop crab1 from going to castle
    - added a crab0 for the start of the game
    - added a box collider tagged as a castle to keep crab1 from moving to the castle (this should prevent the bringing back castle problem)

--- SandCastle_Defense/Assets/Scripts/CameraFollow.cs
+++ SandCastle_Defense/Assets/Scripts/CameraFollow.cs
@@ -10,22 +10,13 @@ public class CameraFollow : MonoBehaviour
 	private Vector3 cameraPos = new Vector3();
 
 	void Start()
-	{
-		// get player by tag
-		/*if (GameObject.FindGameObjectWithTag("Player") != null)
-		{
-			player = GameObject.FindGameObjectWithTag("Player");
-		}*/
-
+	{ 
 		cameraPos.y = this.transform.position.y;
 	}
 
 	// LateUpdate is called after all other Scene Object scripts finish their Updates
 	void LateUpdate()
 	{
-		/*Vector3 newPos = new Vector3((player.transform.position.x + offset.x), transform.position.y, transform.position.z);
-		transform.position = newPos;*/
-		
 		if (Vector2.Distance(player.transform.position, transform.position) > 12)
 		{
 			Vector2 delta_position;
