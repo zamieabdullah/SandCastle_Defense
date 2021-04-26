48469f6c4276bcc01b75b7dfcb5c5c56db283959
emai-lai <emai.lai@tufts.edu>
2021-04-26 17:20:41 -0400

    reset timer and added level conditions
    GracePeriod and CrabAttack varies per level; WaterAttack yet to come

--- SandCastle_Defense/Assets/Scripts/CameraFollow.cs
+++ SandCastle_Defense/Assets/Scripts/CameraFollow.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //public GameObject player; // reference to player object
-    //private Vector3 offset; // distance we want to maintain from the player
-    //private Vector2 player_prev_position;
-    //private Vector3 cameraPos = new Vector3();
+    public GameObject player; // reference to player object
+    private Vector3 offset; // distance we want to maintain from the player
+    private Vector2 player_prev_position;
+    private Vector3 cameraPos = new Vector3();
 
     //changes to the camera follow:
-    public Transform target;
-    public float smoothTime = 0.3f;
-    private Vector3 velocity = Vector3.zero;
+    // public Transform target;
+    // public float smoothTime = 0.3f;
+    // private Vector3 velocity = Vector3.zero;
 
 	void Start()
 	{ 
-		//cameraPos.y = this.transform.position.y;
+		cameraPos.y = this.transform.position.y;
 	}
 
 	// LateUpdate is called after all other Scene Object scripts finish their Updates
 	void LateUpdate()
 	{
-        /*
+        
 		if (Vector2.Distance(player.transform.position, transform.position) > 12)
 		{
 			Vector2 delta_position;
@@ -30,9 +30,10 @@ public class CameraFollow : MonoBehaviour
 			delta_position.y = cameraPos.y;
 			transform.Translate(delta_position);
 		}
-		player_prev_position = player.transform.position;*/
+		player_prev_position = player.transform.position;
         //changes to the camera follow:
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0-2, -10));
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        // Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0-2, -10));
+        // transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
--- SandCastle_Defense/Assets/Scripts/Timer.cs
+++ SandCastle_Defense/Assets/Scripts/Timer.cs
@@ -17,76 +17,45 @@ public class Timer : MonoBehaviour
     //public PlayerController pc;
     //public GameObject centerTower;
 
+    private float currentLevel = 1;
 
     // PRIVATE DECLARATIONS
     private TimeSpan timePlaying;        //TimeSpan part of System namespace
                                          // used to format Time better
 
-    private bool timerIsRunning;         //true when game starts, false when ends
+    //private bool timerIsRunning = false;         //true when game starts, false when ends
 
     //private float timeLeft;           //holds Time.deltaTime value
     public float timeLeft;
 
     public GameObject crab;
+
+
     Vector2 whereToSpawn;
 
-    private void Awake()
-    {
-        instance = this;
-    }
-
+    // private void Awake()
+    // {
+    //     instance = this;
+    // }
 
     private void Start()
     {
         countDisplay.text = "Wave1: 01:00.00";
-        timerIsRunning = false;
-        BeginTimer();
 
-        //crabMovement = GetComponent<CrabMovement>();
-        //crabMovement.centerTower = centerTower;
-        //crabMovement.pc = pc;
+        PlayLevelX(currentLevel);
 
+        //BeginTimer(currentLevel);
+        
     }
 
-
-    public void BeginTimer()
+    private void Update()
     {
-        timerIsRunning = true;
-        timeLeft = 90f; 
-
-        StartCoroutine(UpdateTimer());
-    }
-
-    public void EndTimer()
-    {
-        timerIsRunning = false;
-        SceneManager.LoadScene("WinScene");
-    }
-
-
-    private IEnumerator UpdateTimer()
-    {
-
-
-        while (timerIsRunning)
+        if (timeLeft > 0)
         {
             timeLeft -= Time.deltaTime;
-
-
             timePlaying = TimeSpan.FromSeconds(timeLeft);
-            if (timeLeft >= 0.00)
-            {
-                countDisplay.text = "Wave Duration: " + timePlaying.ToString("mm':'ss");
-            }
-
-            // THIS IS WHERE WE DETERMINE CRAB WAVES... one crab per 0.01
-            if ((timeLeft >= 59.96 & timeLeft <= 60.00) | (timeLeft >= 39.96 & timeLeft <= 40.00) | (timeLeft >= 19.96 & timeLeft <= 20.00))
-            {
-                Debug.Log("spawn crabs");
-                SpawnCrab();
-                
-            }
-
+ 
+            countDisplay.text = "Wave Duration: " + timePlaying.ToString("mm':'ss");
             if (timeLeft <= 15.00)
             {
                 if (timeLeft <= 5.00)
@@ -98,18 +67,156 @@ public class Timer : MonoBehaviour
                     countDisplay.color = Color.yellow;
                 }
             }
-            if (timeLeft <= 0.00)
-            {
-                Debug.Log("timer ended");
-                EndTimer();
-            }
-           
-            yield return null;
         }
+        else 
+        {
+            PlayLevelX(currentLevel++);
+        }
+    }
 
+    private void PlayLevelX(float currentLevel)
+    {   
+        timeLeft = 30 + (5*currentLevel);
 
+        StartCoroutine(GracePeriod());
+        
+        WaterAttack();
 
     }
+
+    IEnumerator GracePeriod() // RIGHT NOW THIS GRACE PERIOD ISN'T WAITING
+    {
+        Debug.Log("grace period");
+        float wait_time = 10 + (2 * currentLevel);
+        yield return new WaitForSeconds(wait_time);
+
+        CrabsAttack();
+        //GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    private void CrabsAttack()
+    {
+        float crabCountToSpawn = (5 * currentLevel);
+        for (int i = 0; i < crabCountToSpawn; i++)
+        {
+            SpawnCrab(); // right now they all spawn at the same time but fix later
+        }
+    }
+
+    private void WaterAttack()
+    {
+        //coming soon
+    }
+
+
+
+    // public void BeginTimer(float level)
+    // {
+    //     //timerIsRunning = true;
+    //     timeLeft = (5 * level); // CHANGE THIS AS NECESSARY 
+
+
+    //     PlayLevelX(currentLevel);
+    //     StartCoroutine(UpdateTimer());
+    // }
+
+    // public void EndTimer()
+    // {
+    //     //timerIsRunning = false;
+
+    //     Debug.Log("timer ended");
+    //     BeginTimer(currentLevel++);
+    //     // timerIsRunning = false;
+    //     // SceneManager.LoadScene("WinScene");
+    // }
+
+
+    // IEnumerator PlayLevelX(float currentLevel)
+    // {
+    //     GracePeriod(currentLevel);
+    //     CrabsAttack(currentLevel);
+    //     WaterAttack(currentLevel);
+
+    //     yield return null;
+    // }
+
+    // IEnumerator GracePeriod(float currentLevel)
+    // {
+    //     Debug.Log("grace period");
+    //     float wait_time = 10 + (2 * currentLevel);
+    //     yield return new WaitForSeconds(wait_time);
+    //     //GetComponent<BoxCollider2D>().enabled = true;
+    // }
+
+    // IEnumerator CrabsAttack(float currentLevel)
+    // {
+    //    // make 5 * currentLevel # crabs attack across ___ seconds
+
+    //     Debug.Log("crabs attack");
+        
+    
+    //     float crabCountToSpawn = (5 * currentLevel);
+    //     for (int i = 0; i < crabCountToSpawn; i++)
+    //     {
+    //         SpawnCrab(); // right now they all spawn at the same time but fix later
+    //     }
+
+    //     yield return null;
+    // }
+     
+    // IEnumerator WaterAttack(float currentLevel)
+    // {
+    //     // coming soon
+    //     yield return null;
+    // }
+
+
+
+
+    // private IEnumerator UpdateTimer()
+    // {
+    //     while (timeLeft > 0)
+    //     {
+    //         timeLeft -= Time.deltaTime;
+
+    //         timePlaying = TimeSpan.FromSeconds(timeLeft);
+    //         if (timeLeft >= 0.00)
+    //         {
+    //             countDisplay.text = "Wave Duration: " + timePlaying.ToString("mm':'ss");
+    //         }
+
+    //         // THIS IS WHERE WE DETERMINE CRAB WAVES... one crab per 0.01
+    //         // if ((timeLeft >= 59.96 & timeLeft <= 60.00) | (timeLeft >= 39.96 & timeLeft <= 40.00) | (timeLeft >= 19.96 & timeLeft <= 20.00))
+    //         // {
+    //         //     Debug.Log("spawn crabs");
+    //         //     SpawnCrab();
+                
+    //         // }
+
+    //         if (timeLeft <= 15.00)
+    //         {
+    //             if (timeLeft <= 5.00)
+    //             {
+    //                 countDisplay.color = Color.red;
+    //             }
+    //             else
+    //             {
+    //                 countDisplay.color = Color.yellow;
+    //             }
+    //         }
+
+    //         if (timeLeft <= 0.00)
+    //         {
+    //             EndTimer();
+                
+               
+    //         }
+            
+    //         yield return null;
+            
+    //     }
+
+    // }
 
     void SpawnCrab()
     {
@@ -117,6 +224,5 @@ public class Timer : MonoBehaviour
 
         Instantiate(crab, whereToSpawn, Quaternion.identity);
     }
-
 
 }
