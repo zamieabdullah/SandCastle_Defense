1f19db60fbc2eedc733d50e2865d62e0eddf0ea1
pmelga01 <pamela.melgar@tufts.edu>
2021-05-05 21:05:39 -0400

    Seagull Animation Controller - flies when wave approaches

---
+++ SandCastle_Defense/Assets/Scripts/Seagull.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seagull : MonoBehaviour
+{
+    public Animator animator;
+    private bool in_flight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        in_flight = false;
+        animator.SetBool("is_flying", in_flight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("wave"))
+        {
+                in_flight = true;
+                animator.SetBool("is_flying", in_flight);
+          
+        }
+        else
+        {
+            in_flight = false;
+            animator.SetBool("is_flying", in_flight);
+        }
+    }
+}
